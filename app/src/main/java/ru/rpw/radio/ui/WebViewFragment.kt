6e787a13b2a34d141bc605ac57fbd46861a3a06b
TT <timofeysin@gmail.com>
2020-01-24 14:08:51 +0300

    Add Settings for change where open url from web pages

--- app/src/main/java/ru/rpw/radio/ui/WebViewFragment.kt
+++ app/src/main/java/ru/rpw/radio/ui/WebViewFragment.kt
@@ -1,13 +1,17 @@
 package ru.rpw.radio.ui
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_web_view.*
 import ru.rpw.radio.R
+
 
 class WebViewFragment : Fragment() {
 
@@ -24,13 +28,26 @@ class WebViewFragment : Fragment() {
         super.onResume()
 
         arguments?.let {
-            val arg = it.get("urlWebView")as Int
+            val arg = it.get("urlWebView") as Int
             mainWebView.loadUrl(getString(arg))
 
-            if (it.getBoolean("JavaScriptEnable")){
+            if (it.getBoolean("JavaScriptEnable")) {
                 mainWebView.settings.javaScriptEnabled = true
                 mainWebView.settings.allowFileAccess = true
             }
+            when (getSharedPreference(mainWebView.context, "AppForOpenURL")) {
+                "APP" -> {
+                    mainWebView.webViewClient = WebViewClient()
+                }
+                "CHROME" -> {
+                    mainWebView.webChromeClient = WebChromeClient()
+                }
+            }
         }
+    }
+
+    private fun getSharedPreference(context: Context, preferenceName: String): String? {
+        val prefs = context.getSharedPreferences(preferenceName, Context.MODE_PRIVATE)
+        return prefs.getString(preferenceName, "NULL")
     }
 }
